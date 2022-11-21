7316ea9380d042cacd3635b0b30cac51dacb2473
JuliaZorc <118739132+JuliaZorc@users.noreply.github.com>
2022-11-21 08:09:47 -0500

    Update Product.swift
    updated description default

--- New Wave Store/Model/Product.swift
+++ New Wave Store/Model/Product.swift
@@ -14,7 +14,7 @@ class Product{
     var quantity : Int
     var price : Double
     
-    init(name : String = "", description : String = "", picture : String = "", quantity : Int = 0, price : Double = 0.0){
+    init(name : String = "", description : String = "New Wave's worst computer", picture : String = "", quantity : Int = 0, price : Double = 0.0){
         self.name = name
         self.description = description
         self.picture = picture
